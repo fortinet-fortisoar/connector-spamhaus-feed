7abb0a33541eaf0715017903101cf271b1ba14e8
cs-shubham-morankar <smorankar@fortinet.com>
2022-03-15 12:25:20 +0530

    Updated connector code using IP list

--- spamhaus-feed/operations.py
+++ spamhaus-feed/operations.py
@@ -4,7 +4,7 @@
   FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
   Copyright end """
 
-import requests, json, datetime, time
+import requests, datetime, time
 from connectors.core.connector import get_logger, ConnectorError
 
 try:
@@ -23,24 +23,31 @@ errors = {
     500: 'Internal Server Error'
 }
 
+MONTH_LIST = {
+    'Jan': '-01-',
+    'Feb': '-02-',
+    'Mar': '-03-',
+    'Apr': '-04-',
+    'May': '-05-',
+    'Jun': '-06-',
+    'Jul': '-07-',
+    'Aug': '-08-',
+    'Sep': '-09-',
+    'Oct': '-10-',
+    'Nov': '-11-',
+    'Dec': '-12-'
+}
+
 
 class SpamhausFeed(object):
     def __init__(self, config, *args, **kwargs):
-        self.username = config.get('username')
-        self.password = config.get('password')
-        url = config.get('server_url').strip('/')
-        if not url.startswith('https://') and not url.startswith('http://'):
-            self.url = 'https://{0}/'.format(url)
-        else:
-            self.url = url + '/'
-        self.token = self.login(self.url, self.username, self.password)
+        self.url = config.get('service')
         self.sslVerify = config.get('verify_ssl')
 
-    def make_rest_call(self, url, method, data=None, params=None):
+    def make_rest_call(self, url, method):
         try:
-            url = self.url + url
-            headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + self.token}
-            response = requests.request(method, url, headers=headers, verify=self.sslVerify, data=data, params=params)
+            url = self.url
+            response = requests.request(method, url, verify=self.sslVerify)
             if response.ok or response.status_code == 204:
                 logger.info('Successfully got response for url {0}'.format(url))
                 if 'json' in str(response.headers):
@@ -48,7 +55,7 @@ class SpamhausFeed(object):
                 else:
                     return response.content
             elif response.status_code == 404:
-                return {'results': []}
+                return {'blocklist_ips': []}
             else:
                 logger.error("{0}".format(errors.get(response.status_code, '')))
                 raise ConnectorError("{0}".format(errors.get(response.status_code, response.text)))
@@ -64,31 +71,6 @@ class SpamhausFeed(object):
         except Exception as err:
             raise ConnectorError(str(err))
 
-    def login(self, url, username, password):
-        body = {
-            'username': username,
-            'password': password,
-            'realm': 'intel'
-        }
-        endpoint = url + 'api/v1/login'
-        response = requests.post(endpoint, data=json.dumps(body))
-        if response:
-            return response.json().get('token')
-        else:
-            raise ConnectorError("Invalid endpoint or credentials")
-
-
-def check_payload(payload):
-    payload1 = {}
-    for key, value in payload.items():
-        if isinstance(value, dict):
-            nested = check_payload(value)
-            if len(nested.keys()) > 0:
-                payload1[key] = nested
-        elif value:
-            payload1[key] = value
-    return payload1
-
 
 def convert_datetime_to_epoch(date_time):
     d1 = time.strptime(date_time, "%Y-%m-%dT%H:%M:%S.%fZ")
@@ -96,35 +78,32 @@ def convert_datetime_to_epoch(date_time):
     return epoch
 
 
+def fetch_datetime(ip_blacklist):
+    for line in ip_blacklist[:6]:
+        if 'Last-Modified' in line:
+            x = line.split(",")[1].split(" ")
+            date_time = x[3] + MONTH_LIST.get(x[2]) + x[1] + "T" + x[4] + ".000Z"
+            last_modified = convert_datetime_to_epoch(date_time)
+        elif 'Expires' in line:
+            x = line.split(",")[1].split(" ")
+            date_time = x[3] + MONTH_LIST.get(x[2]) + x[1] + "T" + x[4] + ".000Z"
+            expires = convert_datetime_to_epoch(date_time)
+    return last_modified, expires
+
+
 def fetch_indicators(config, params, **kwargs):
     try:
         sf = SpamhausFeed(config)
-        endpoint = "api/intel/v1/byobject/cidr/{0}/{1}/{2}/{3}".format(params.get('dataset'),
-                                                                       params.get('mode').lower(),
-                                                                       params.get('type').lower(),
-                                                                       params.get('ipaddress'))
-        start_time = params.get('start_time')
-        if start_time:
-            start_time = convert_datetime_to_epoch(start_time)
-        end_time = params.get('end_time')
-        if end_time:
-            end_time = convert_datetime_to_epoch(end_time)
-        payload = {
-            'limit': params.get('limit'),
-            'since': start_time,
-            'until': end_time
-        }
-        payload = check_payload(payload)
-        response = sf.make_rest_call(endpoint, 'GET', params=payload)
-        response = response.get('results')
-        mode = params.get('output_mode')
-        if mode == 'Create as Feed Records in FortiSOAR':
-            create_pb_id = params.get("create_pb_id")
-            trigger_ingest_playbook(response, create_pb_id, parent_env=kwargs.get('env', {}),
-                                    batch_size=1000, dedup_field="pattern")
-            return {"message": "Succesfully triggered playbooks for creating feed records"}
-        else:
-            return response
+        endpoint = ""
+        ip_blacklist_list = []
+        response = sf.make_rest_call(endpoint, 'GET')
+        if response:
+            ip_blacklist = str(response).split("\\n")
+            last_modified, expires = fetch_datetime(ip_blacklist)
+            for ip in ip_blacklist[4:-1]:
+                ip_type = ip.split(";")
+                ip_blacklist_list.append({'ip': ip_type[0].strip(), 'type': ip_type[1].strip(), 'last_modified': last_modified, 'expires': expires})
+            return ip_blacklist_list
     except Exception as err:
         logger.exception("{0}".format(str(err)))
         raise ConnectorError("{0}".format(str(err)))
